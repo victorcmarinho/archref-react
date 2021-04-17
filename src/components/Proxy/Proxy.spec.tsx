5045464e0433d61a1271c638c445e6ec07710bf0
Victor Marinho <victorcmarinho@gmail.com>
2021-04-17 19:29:06 -0300

    update unit test

--- src/components/Proxy/Proxy.spec.tsx
+++ src/components/Proxy/Proxy.spec.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { act, render } from '@testing-library/react';
-import axios from 'axios';
-import MockAdapter from 'axios-mock-adapter';
+import { AxiosResponse } from 'axios';
 import * as ThemeContext from 'hooks/ThemeContext';
+import * as resquestFunctions from 'services/requestExample';
 
 import { IGitHubUsers, Type } from './interfaces';
 import { Proxy } from './Proxy';
+
+jest.mock('services/requestExample');
 
 describe('<Proxy/>', () => {
   const toggle = jest.fn();
@@ -23,33 +25,43 @@ describe('<Proxy/>', () => {
   });
 
   it('should render', () => {
-    const mock = new MockAdapter(axios);
-    const mockValue: IGitHubUsers[] = [
-      {
-        login: 'mojombo',
-        id: 1,
-        node_id: 'MDQ6VXNlcjE=',
-        avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
-        gravatar_id: '',
-        url: 'https://api.github.com/users/mojombo',
-        html_url: 'https://github.com/mojombo',
-        followers_url: 'https://api.github.com/users/mojombo/followers',
-        following_url:
-          'https://api.github.com/users/mojombo/following{/other_user}',
-        gists_url: 'https://api.github.com/users/mojombo/gists{/gist_id}',
-        starred_url:
-          'https://api.github.com/users/mojombo/starred{/owner}{/repo}',
-        subscriptions_url: 'https://api.github.com/users/mojombo/subscriptions',
-        organizations_url: 'https://api.github.com/users/mojombo/orgs',
-        repos_url: 'https://api.github.com/users/mojombo/repos',
-        events_url: 'https://api.github.com/users/mojombo/events{/privacy}',
-        received_events_url:
-          'https://api.github.com/users/mojombo/received_events',
-        type: Type.User,
-        site_admin: false,
-      },
-    ];
-    mock.onGet('/users').reply(200, mockValue);
+    const axiosResponse: AxiosResponse<IGitHubUsers[]> = {
+      data: [
+        {
+          login: 'mojombo',
+          id: 1,
+          node_id: 'MDQ6VXNlcjE=',
+          avatar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+          gravatar_id: '',
+          url: 'https://api.github.com/users/mojombo',
+          html_url: 'https://github.com/mojombo',
+          followers_url: 'https://api.github.com/users/mojombo/followers',
+          following_url:
+            'https://api.github.com/users/mojombo/following{/other_user}',
+          gists_url: 'https://api.github.com/users/mojombo/gists{/gist_id}',
+          starred_url:
+            'https://api.github.com/users/mojombo/starred{/owner}{/repo}',
+          subscriptions_url:
+            'https://api.github.com/users/mojombo/subscriptions',
+          organizations_url: 'https://api.github.com/users/mojombo/orgs',
+          repos_url: 'https://api.github.com/users/mojombo/repos',
+          events_url: 'https://api.github.com/users/mojombo/events{/privacy}',
+          received_events_url:
+            'https://api.github.com/users/mojombo/received_events',
+          type: Type.User,
+          site_admin: false,
+        },
+      ],
+      config: {},
+      headers: {},
+      status: 200,
+      statusText: '200',
+    };
+
+    const promise = new Promise<AxiosResponse<IGitHubUsers[]>>(resolver =>
+      resolver(axiosResponse),
+    );
+    jest.spyOn(resquestFunctions, 'getExample').mockReturnValue(promise);
 
     act(() => {
       const component = render(<Proxy />);
